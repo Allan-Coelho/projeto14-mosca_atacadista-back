fc1275249e06654ddc3f657d94c13afdfefd040a
Allan Vieira Coelho <github@allanvieira.dev>
2022-09-19 15:44:02 -0300

    feat(product): the POST method was implemented.

---
+++ src/schemas/productSchema.js
@@ -0,0 +1,10 @@
+import Joi from "joi";
+
+const newProductSchema = Joi.object({
+  name: Joi.string().min(1).required(),
+  description: Joi.string().min(256).max(1024).required(),
+  price: Joi.number().min(0.01).multiple(0.01).required(),
+  pictures: Joi.array().min(3).max(5).items(Joi.string().uri()).required(),
+});
+
+export { newProductSchema };
